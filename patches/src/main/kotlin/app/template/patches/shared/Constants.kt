f523bf223995890c7725da65c147c8f1453d7362
wyqtt <wyattmimportant@gmail.com>
2026-08-07 20:00:19 -0300

    kwgt test

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -5,43 +5,12 @@ import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 object Constants {
-    val COMPATIBILITY_EXAMPLE = Compatibility(
-        name = "XYZ app", // App name as it appears in the Android launcher.
-        packageName = "com.example.app",
-        apkFileType = ApkFileType.APK, // Preferred or recommended file type.
-        appIconColor = 0xFF0045, // Icon color in Morphe Manager. Usually the same color as the icon background.
-        targets = listOf(
-            // "version = null" means the patch works with the latest app target
-            // and is expected to work with all future app targets.
-            //
-            // It is highly recommended to always include the exact app version you developed your patches for
-            // or the last version you have confirmed as 100% working.
-            AppTarget(
-                version = "2.0.0"
-            ),
-            AppTarget(
-                version = "1.0.2"
-            )
-        )
-    )
-
-
-    val COMPATIBILITY_EXAMPLE_2 = Compatibility(
-        name = "ABC app",
-        packageName = "com.example.app",
-        apkFileType = ApkFileType.APKM,
-        appIconColor = 0x00FF45, // Icon color in Morphe Manager. Usually the same color as the icon background.
-        targets = listOf(
-            // 'any' version supported experimentally.
-            AppTarget(
-                version = null,
-                isExperimental = true
-            ),
-            // App version confirmed 100% working.
-            AppTarget(
-                version = "1.0.2"
-            )
-        )
+    
+    val KWGT_COMPATIBILITY = Compatibility(
+        name = "KWGT (Kustom Widget Maker)",
+        packageName = "org.kustom.widget",
+        appIconColor = 0x4CAF50,
+        targets = listOf(AppTarget(version = "3.82b619510aosp", versionCode = 382619510))
     )
 
 }
